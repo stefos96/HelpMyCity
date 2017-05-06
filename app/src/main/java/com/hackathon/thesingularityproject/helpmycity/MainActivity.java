ccf138ffe24ae1a1015f1c7cbdaff5a19ff26e7c
stefos96 <stefos96@hotmail.com>
2017-05-06 23:44:36 +0300

    Fix crash in case of no internet connection

--- app/src/main/java/com/hackathon/thesingularityproject/helpmycity/MainActivity.java
+++ app/src/main/java/com/hackathon/thesingularityproject/helpmycity/MainActivity.java
@@ -1,5 +1,11 @@
 package com.hackathon.thesingularityproject.helpmycity;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -18,8 +24,36 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View v) {
-        Intent MainView;
-        MainView = new Intent(this, ViewAllProblems.class);
-        startActivity(MainView);
+        if (isNetworkStatusAvialable (getApplicationContext())) {
+            Intent MainView;
+            MainView = new Intent(this, ViewAllProblems.class);
+            startActivity(MainView);
+        }
+        else{
+            AlertDialog alertDialog = new AlertDialog.Builder(this).create();
+            alertDialog.setTitle("No internet connection");
+            alertDialog.setMessage("Please open wifi or mobile data.");
+            alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+                    new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                        }
+                    });
+            alertDialog.show();
+        }
+
+    }
+
+    public static boolean isNetworkStatusAvialable (Context context) {
+        ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        if (connectivityManager != null)
+        {
+            NetworkInfo netInfos = connectivityManager.getActiveNetworkInfo();
+            if(netInfos != null)
+                if(netInfos.isConnected())
+                    return true;
+        }
+        return false;
     }
 }
+
