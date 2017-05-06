535ec46b541bc28de506624e18c8e20422441608
ApolloTkog <zkaragiannis@yahoo.gr>
2017-05-06 10:52:28 +0300

    I added a comment as well

--- app/src/main/java/com/hackathon/thesingularityproject/helpmycity/MainActivity.java
+++ app/src/main/java/com/hackathon/thesingularityproject/helpmycity/MainActivity.java
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //this is the first commit I've ever done on this stupid framework because I need to test it out
+        //STFU....... 
     }
 }
