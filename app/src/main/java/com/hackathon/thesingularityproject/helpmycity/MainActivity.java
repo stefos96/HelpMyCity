ccfb224b4e9dc5e582c4977bb39497e7c1cf030a
Trojanzaro <anastasiszaro@gmail.com>
2017-05-06 11:02:50 +0300

    THIS IS THE FINAL TESTING COMMIT BEFORE WE START

--- app/src/main/java/com/hackathon/thesingularityproject/helpmycity/MainActivity.java
+++ app/src/main/java/com/hackathon/thesingularityproject/helpmycity/MainActivity.java
@@ -9,8 +9,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //this is the first commit I've ever done on this stupid framework because I need to test it out
-        //STFU.......
-        //1234567890
     }
 }
